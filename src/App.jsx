b293395a432fb9219731ffddb5b3cff3be10fc14
Tanim-10 <islammdtanim88@gmail.com>
2025-08-17 23:29:25 +0530

    Added Show Finished feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
 
 function App() {
   const [todo, setTodo] = useState("")
   const [editingId, setEditingId] = useState(null)
+  const [showfinished, setShowfinished] = useState(true)
 
   const [todos, setTodos] = useState(() => {
     const saved = localStorage.getItem("todos")
@@ -50,10 +53,14 @@ function App() {
     setTodos(newtodos)
   }
 
+  const togglefinish = (e) => {
+    setShowfinished(!showfinished)
+  }
+  
   return (
     <>
       <Navbar />
-      <div className="bg-[#A08963] text-white m-4 p-4 rounded-2xl">
+      <div className="bg-[#A08963] text-white m-4 p-4 rounded-2xl w-1/2 mx-auto">
         <div className="addtodo mb-4">
           <h2 className='font-bold mb-1'>Add a ToDo</h2>
           <input 
@@ -63,17 +70,18 @@ function App() {
             type="text" 
           />
           <button 
-            onClick={handleadd} 
-            className='bg-[#C9B194] mx-2 px-2 rounded-lg transition-transform duration-200 hover:scale-105'
+            onClick={handleadd} disabled={todo.length <= 3}
+            className='bg-[#C9B194]  mx-2 px-2 rounded-lg transition-transform duration-200 hover:cursor-pointer hover:scale-105 disabled:bg-[#DBDBDB]'
           >
             {editingId ? "Update" : "Add"}
           </button>
         </div>
+        <input onChange={togglefinish} type="checkbox" name="" checked={showfinished} id="" /> Show Finished
         <h2 className='font-bold'>Your ToDo(s)</h2>
         <div className="todos">
           {todos.length === 0 && <div className='mx-32 animate-pulse text-2xl'>No ToDos</div>}
           {todos.map((item) => {
-            return <div key={item.id} className="todo my-2 flex w-1/4 justify-between">
+            return (showfinished || !item.isCompleted) && <div key={item.id} className="todo my-2 flex w-full justify-between">
               <div className='flex gap-4'>
                 <input 
                   name={item.id} 
@@ -86,15 +94,15 @@ function App() {
               <div className="buttons flex h-full">
                 <button 
                   onClick={(e) => { handleedit(e, item.id) }} 
-                  className='bg-[#C9B194] mx-2 px-2 rounded-lg transition-transform duration-200 hover:scale-105'
+                  className='bg-[#C9B194] mx-2 px-2 py-0.5 rounded-lg transition-transform duration-200 hover:scale-105'
                 >
-                  Edit
+                  <CiEdit size={24} />
                 </button>
                 <button 
                   onClick={(e) => { handledelete(e, item.id) }} 
-                  className='bg-[#C9B194] mx-2 px-2 rounded-lg transition-transform duration-200 hover:scale-105'
+                  className='bg-[#C9B194] mx-2 px-2 py-0.5 rounded-lg transition-transform duration-200 hover:scale-105'
                 >
-                  Delete
+                  <MdDelete size={24}/>
                 </button>
               </div>
             </div>
